be019c97306f745298b1e2ce9c10c258c0693d63
ali frd <alifrd49@gmail.com>
2018-08-16 00:57:05 +0430

    js mvc build

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(() => {
+    
+    const BackEnd = (() =>{
+
+    })();
+
+  
+  
+  
+  
+  
+    const FrontEnd =(() =>{
+    })();
+ 
+
+  
+  
+  
+  
+    const Controller = ((bckEnd,frnEnd) =>{
+    })(BackEnd,FrontEnd);
+})();
