5b36fbaf3e49cbed908b3730aa858ddc802991a5
ali frd <alifrd49@gmail.com>
2018-08-16 00:58:57 +0430

    init , Dom , loadHistroy func addded

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 (() => {
     
     const BackEnd = (() =>{
-
     })();
 
   
@@ -10,6 +9,13 @@
   
   
     const FrontEnd =(() =>{
+        const Dom= {
+            noteID : 'Note_',
+            noteContainer : 'ul',
+            addbtn : '#addSticker',
+            noteDelete : '.Del'
+
+        }
     })();
  
 
@@ -18,5 +24,37 @@
   
   
     const Controller = ((bckEnd,frnEnd) =>{
+        const Dom = frnEnd.getDom() ;
+        
+
+        const setUpEventListner = () => {
+          
+            const Notes = document.querySelectorAll('li');
+            Notes.forEach(element => {
+                element.addEventListener('click',function(e){   modifyNote(e,"edit");   });
+                element.addEventListener('mouseleave',function(e){  modifyNote(e,"set");    });
+            });
+            
+            document.querySelector(Dom.addbtn).addEventListener('click' , addNote )
+            document.querySelector(Dom.noteDelete).addEventListener('click' , function(e) {
+                deleteNote(e);     
+            });
+        
+        };
+
+
+        const loadHistyory = () =>{
+            let perviousNote ;
+            let note = [];
+            perviousNote =  bckEnd.returnAllNote();
+            frnEnd.buildHistory(perviousNote);
+        }
+        const init = (() =>{
+            loadHistyory();    
+            setUpEventListner();
+        
+        });
+
+        init();
     })(BackEnd,FrontEnd);
 })();
